e3c9f8c901fac0045c586f139343d758f5ceaf95
McAroon <pgrabel@gmail.com>
2025-07-09 18:40:53 +0300

    footbingo career break not counted as retired

--- Quingo.Scripts/Footbingo/FootbingoRegex.cs
+++ Quingo.Scripts/Footbingo/FootbingoRegex.cs
@@ -8,11 +8,11 @@ public static partial class FootbingoRegex
     public static Regex ClubRetiredRegex { get; } = CreateClubRetiredRegex();
     public static Regex ClubDuplicatesRegex { get; } = CreateClubDuplicatesRegex();
     
-    [GeneratedRegex("without club|youth|unknown|u\\d+|under|academ|akadem|jgd|jugend|jong|^ban$",
+    [GeneratedRegex("career break|without club|youth|unknown|u\\d+|under|academ|akadem|jgd|jugend|jong|^ban$",
         RegexOptions.IgnoreCase)]
     private static partial Regex CreateExcludeRegex();
 
-    [GeneratedRegex("retired|career break", RegexOptions.IgnoreCase)]
+    [GeneratedRegex("retired", RegexOptions.IgnoreCase)]
     private static partial Regex CreateClubRetiredRegex();
 
     [GeneratedRegex("^(.+) (B|C|D|II|III|IV|2|3|4)$", RegexOptions.IgnoreCase)]
